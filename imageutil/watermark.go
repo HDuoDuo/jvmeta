8cdf373dc8155cc546c9e821469317af1d155be7
Da Hai <me@dahai.com>
2025-01-26 18:20:48 +0800

    watermark image and location

--- imageutil/watermark.go
+++ imageutil/watermark.go
@@ -6,9 +6,9 @@ import (
 )
 
 func Watermark(src image.Image, wmk image.Image, pt image.Point) image.Image {
-	dst := image.NewNRGBA(image.Rect(0, 0,
-		src.Bounds().Dx(), src.Bounds().Dy()))
+	dst := image.NewNRGBA(image.Rect(0, 0, src.Bounds().Dx(), src.Bounds().Dy()))
 	draw.Draw(dst, dst.Bounds(), src, src.Bounds().Min, draw.Src)
-	draw.Draw(dst, dst.Bounds(), wmk, wmk.Bounds().Min.Add(pt), draw.Over)
+	mark := Resize(wmk, 0, src.Bounds().Dy()/9)
+	draw.Draw(dst, mark.Bounds().Add(dst.Bounds().Max.Sub(mark.Bounds().Max.Sub(image.Point{12, 0}))), mark, mark.Bounds().Min.Add(pt), draw.Over)
 	return dst
 }
